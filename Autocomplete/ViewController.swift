a6cd15d30283c0b1f8ebd7400894215cf260de36
ncreated <ncreated@gmail.com>
2017-07-16 10:55:56 +0200

    Initialize project

---
+++ Autocomplete/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Autocomplete
+//
+//  Created by Maciek Grzybowski on 16.07.2017.
+//  Copyright © 2017 ncreated. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
